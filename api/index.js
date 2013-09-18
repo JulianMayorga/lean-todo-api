1e000ffa8b65b9cf72da2b75f090f5fb2d595806
JulianMayorga <mayorga.julian@gmail.com>
2013-09-18 18:55:22 -0300

    Configure hapi to work with heroku

--- api/index.js
+++ api/index.js
@@ -2,11 +2,18 @@ var Hapi = require('hapi');
 
 var routes = require('./routes');
 
+var port = process.env.PORT || 8000;
+
 // Create a server with a host and port
-var server = Hapi.createServer('localhost', 8000);
+// Que significa el + antes de process...?
+var server = new Hapi.Server(+process.env.PORT, '0.0.0.0');
 
 server.addRoutes(routes.endpoints);
 
-server.start();
+// https://github.com/wpreul/hapi-heroku
+server.start(function () {
+	server.settings.uri = process.env.HOST ? 'http://' + process.env.HOST + ':' + process.env.PORT : server.settings.uri;
+	console.log('Server started at: ' + server.settings.uri);
+});
 
 var exports = module.exports = server;
