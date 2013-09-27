7e848f3d48f3f2f50fc415cae9132ade91683f3c
JulianMayorga <mayorga.julian@gmail.com>
2013-09-27 10:34:10 -0300

    [Bug] Use port 8000 in development

--- api/index.js
+++ api/index.js
@@ -4,15 +4,16 @@ var routes = require('./routes');
 
 var port = process.env.PORT || 8000;
 
-// Create a server with a host and port
-// Que significa el + antes de process...?
-var server = new Hapi.Server(+process.env.PORT, '0.0.0.0');
+// Pregunta: Que significa el + antes de process...?
+// Respuesta: El + antes de una variable retorna su representacion numerica
+// Referencia: http://stackoverflow.com/questions/6682997/javascript-plus-symbol-before-variable
+var server = new Hapi.Server(+port, '0.0.0.0');
 
 server.addRoutes(routes.endpoints);
 
-// https://github.com/wpreul/hapi-heroku
+// Integrar hapi con heroku: https://github.com/wpreul/hapi-heroku
 server.start(function () {
-	server.settings.uri = process.env.HOST ? 'http://' + process.env.HOST + ':' + process.env.PORT : server.settings.uri;
+	server.settings.uri = process.env.HOST ? 'http://' + process.env.HOST + ':' + process.env.PORT : server.info.uri;
 	console.log('Server started at: ' + server.settings.uri);
 });
 
